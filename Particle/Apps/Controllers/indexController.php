327b828e8ed0dfdf1db2193e7279ad02fa8b6f60
Guillermo Cespedes <gcespedes@grupolfmedia.com>
2018-04-10 15:46:25 -0300

    General adjustments

--- Particle/Apps/Controllers/indexController.php
+++ Particle/Apps/Controllers/indexController.php
@@ -3,6 +3,7 @@
 namespace Particle\Apps\Controllers;
 
 use Particle\Core;
+use Particle\Apps\Entities;
 
 class indexController extends Core\Controller
 {
@@ -14,7 +15,7 @@ class indexController extends Core\Controller
     public function index()
     {
         $spot = parent::$spotInstance;
-        $personMapper = $spot->mapper('\Particle\Apps\Entities\Person');
+        $personMapper = $spot->mapper('Entities\Person');
         $personMapper->migrate();
         $this->view->show();
     }
