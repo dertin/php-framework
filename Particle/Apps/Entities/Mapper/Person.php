2729a60aa26c1d711deb86585ece8c51bc52165e
Mateo Mujica <mateo.mujica@tufiesta.com.uy>
2018-04-12 12:39:44 -0300

    test custom mapper

--- Particle/Apps/Entities/Mapper/Person.php
+++ Particle/Apps/Entities/Mapper/Person.php
@@ -8,7 +8,7 @@ class Persona extends Mapper
 {
     public function libros1eraEdicion()
     {
-        return $this->query('SELECT * FROM person INNER JOIN book ON book.PersonId = Person.personId
+        return $this->query('SELECT * FROM person INNER JOIN book ON book.PersonId = person.PersonId
                               WHERE book.BookEdition = 1');
     }
 }
