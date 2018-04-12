67dd2df4723c2f4767adaf3a3fb1e39ab18d1454
Mateo Mujica <mateo.mujica@tufiesta.com.uy>
2018-04-12 13:12:25 -0300

    autoload changes

--- Particle/Core/autoload/autoload_apps.php
+++ Particle/Core/autoload/autoload_apps.php
@@ -10,26 +10,30 @@ if (!defined('PARTICLE_PATH_APPS')) {
  * Autoload de Entidades de base de datos con Spot ORM
  */
 spl_autoload_register(function ($ClassNameWithNameSpace) {
-    $namespace = 'Particle\Apps\Entities\\';
 
-    if (substr($ClassNameWithNameSpace, 0, strlen($namespace)) === $namespace) {
-        $nameClass = substr($ClassNameWithNameSpace, strlen($namespace));
+    $aNamespace = array('Particle\Apps\Entities\\', 'Particle\Apps\Entities\Mapper\\');
 
-        $filePath = PARTICLE_PATH_APPS.'Entities'.DS.$nameClass.'.php';
-        
-        if (!is_readable($filePath)) {
-            foreach (glob(PARTICLE_PATH_APPS.'Entities'.DS.'*', GLOB_ONLYDIR) as $dir) {
-                $subDir= basename($dir);
-                $filePath = PARTICLE_PATH_APPS.'Entities'.DS.$subDir.DS.$nameClass.'.php';
-                if (is_readable($filePath)) {
-                    require_once $filePath;
-                    break;
+    foreach ($aNamespace as $sNamespace) {
+        if (substr($ClassNameWithNameSpace, 0, strlen($sNamespace)) === $sNamespace) {
+            $nameClass = substr($ClassNameWithNameSpace, strlen($namespace));
+
+            $filePath = PARTICLE_PATH_APPS.'Entities'.DS.$nameClass.'.php';
+
+            if (!is_readable($filePath)) {
+                foreach (glob(PARTICLE_PATH_APPS.'Entities'.DS.'*', GLOB_ONLYDIR) as $dir) {
+                    $subDir= basename($dir);
+                    $filePath = PARTICLE_PATH_APPS.'Entities'.DS.$subDir.DS.$nameClass.'.php';
+                    if (is_readable($filePath)) {
+                        require_once $filePath;
+                        break;
+                    }
                 }
+            } else {
+                require_once $filePath;
             }
-        } else {
-            require_once $filePath;
         }
     }
+
     return true;
 });
 
