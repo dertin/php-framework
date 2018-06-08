0e5b5456decce1a0a9b4e2eb49b5ded0ce9f9fd7
Guillermo Cespedes <gcespedes@grupolfmedia.com>
2018-06-08 17:46:51 -0300

    Sample testing function to send Request with POST data

--- Particle/Apps/phpunit/tests/indexTest.php
+++ Particle/Apps/phpunit/tests/indexTest.php
@@ -17,36 +17,21 @@ final class TestingControllerTest extends TestCase
     {
         $this->view = new \Smarty();
     }
-
-    // public function testBootstrapDefault()
-    // {
-    //     try {
-    //         Particle\Core\Bootstrap::run(new Particle\Core\Request('testing/testing'));
-    //         $this->assertTrue(true);
-    //     } catch (\Exception $e) {
-    //         $this->fail('Exception: '.$e->getMessage());
-    //     }
-    // }
-    //
-    // public function testSumar()
-    // {
-    //     $cSuma = new Particle\Apps\testingController;
-    //
-    //     $resut = $cSuma->suma(2, 1);
-    //
-    //     $this->assertEquals(3, $resut);
-    // }
-
-    public function testORM()
+    /**
+     * Sample testing function to send Request with POST data
+     * @return void
+     */
+    public function testRequestPOST()
     {
         try {
-            $objIndexController = new Controllers\indexController;
-            //$aResult = $objIndexController->testORM();
-
-            $this->assertInternalType(Constraint\IsType::TYPE_INT, $aResult['PersonId']);
-            $this->assertNotEmpty($aResult['PersonId']);
-
-            $this->assertNotEmpty($aResult['BookTitle']);
+            // Configuration request with POST parameters a = 1, b = 2
+            $objRequestIndex = new Core\Request('index/index', 'POST', array('a'=>1,'b'=>2));
+            // Execute the Request, simulate the execution of a web request to the configured Request
+            Core\Bootstrap::run($objRequestIndex);
+            // Clean super global variable [GET and POST] configured for the previous Request
+            $objRequestIndex->closeRequestTesting();
+            // Verify that the request performs the action by printing a json, changing some value in DB or printing an HTML
+            $this->assertTrue(true);
         } catch (\Exception $e) {
             $this->fail('Exception: '.$e->getMessage());
         }
